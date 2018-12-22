a9201b261c9556c36d79e403aae6f8ec9f176d3c
Marcos Aguayo <marcos@aguayo.es>
2018-12-22 02:01:57 +0000

    Update settings.py

--- monerocity/settings.py
+++ monerocity/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'monerocity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': '',
     }
 }
 
